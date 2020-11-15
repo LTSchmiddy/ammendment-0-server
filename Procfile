73aa70bbe3f15eb7b94fc700bea2fd1bb7d1c1dc
LT_Schmiddy <schmidalex1996@gmail.com>
2020-11-14 20:02:14 -0500

    Adding Heroku Support

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python -c "import cython_out.a_zero; a_zero.main()"
---
+++ src/py/a_zero_flaskapp.py
@@ -0,0 +1,27 @@
+import sys
+import multiprocessing
+import time
+
+ordinal = lambda n: "%d%s" % (n, "tsnrhtdd"[(n // 10 % 10 != 1) * (n % 10 < 4) * n % 10::4])
+
+should_quit = False
+
+def construct_app():
+    from utils import std_handler
+    std_handler.init()
+
+    if sys.platform.startswith("win"):
+        # On Windows calling this function is necessary.
+        multiprocessing.freeze_support()
+
+    import settings.paths
+    settings.load_settings()
+
+
+    import server
+    import db
+    db.init()
+
+    return server.flask_site.app
+
+app = construct_app()
